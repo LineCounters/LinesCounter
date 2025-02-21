b32b88e927d36f50d2cfb58520b3845f5052ddee
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2025-02-20 19:04:25 -0600

    feat: :sparkles: counts lines according to the entire counting standard

--- src/LineBuilder.py
+++ src/LineBuilder.py
@@ -16,27 +16,49 @@ class LineBuilder:
         while i < len(self._raw_lines):
             line: str = self._raw_lines[i]
             stripped: str = line.strip()
-            if self._is_comment(stripped):
+
+            if self._is_nothing(stripped) or self._is_single_comment(stripped):
+                pass
+            elif self._is_multiline_comment(stripped):
                 i = self._skip_comment(i)
             elif self._is_method_signature(stripped):
-                _physical_Line : int = self._count_physical_lines(":",i) 
-                i = _physical_Line - 1 + i
-                result.append(CodeLine("method signature",_physical_Line, 1))
+                result.append(CodeLine("method signature",1, 1))
+            elif self._is_conditional(stripped):
+                result.append(CodeLine("Conditional",1, 1))
+            elif self._is_loop(stripped):
+                result.append(CodeLine("Loop",1, 1))
+            elif self._is_try_except(stripped):
+                result.append(CodeLine("try_except",1, 1))
             elif self._is_import_line(stripped):
                 result.append(CodeLine("import",1, 1))
+            elif self._is_with_statement(stripped):
+                result.append(CodeLine("with",1, 1))
             elif self._is_class(stripped):
                 result.append(CodeLine("class", 1, 1))
             elif self._is_variable(stripped) or self._is_self_attribute(stripped):
-                result.append(CodeLine("variable", 1, 1))
-            elif self._is_return(stripped):
-                _physical_Line : int = 1 if stripped.count("(") == 0 else self._count_physical_lines(")",i)
-                i = _physical_Line - 1 + i
-                result.append(CodeLine("return", _physical_Line, 0))
+                    logical_count = stripped.count(";") + 1 
+                    result.append(CodeLine("variable", 1, logical_count))
+            elif self._is_control_statement(stripped):
+                result.append(CodeLine("ControlStatement", 1, 1))
+            else:
+                result.append(CodeLine("SomethingElse", 1, 0))
             i += 1
         return result
     
 
+    def _is_with_statement(self,stripped:str)->bool:
+        return stripped.startswith("with")
+
+
+    def _is_try_except(self,stripped:str):
+        return stripped.startswith("try") or stripped.startswith("except") or stripped.startswith("finally") or stripped.startswith("raise") 
+
+    def _is_loop(self,stripped:str) -> bool:
+        return stripped.startswith("for") or stripped.startswith("while")
     
+    def _is_conditional(self,stripped : str) -> bool:
+        return stripped.startswith("if") or stripped.startswith("elif") or stripped.startswith("else") or stripped.startswith("assert") 
+
     def _is_import_line(self, stripped: str) -> bool:
         starts_with_import: bool = stripped.startswith("import")
         starts_with_from: bool = stripped.startswith("from")
@@ -48,11 +70,11 @@ class LineBuilder:
     def _is_method_signature(self, stripped: str) -> bool:
         return stripped.startswith("def ")
     
-    def _is_return(self, stripped: str) -> bool:
-        return stripped.startswith("return")
+    def _is_control_statement(self, stripped: str) -> bool:
+        return stripped.startswith("return") or stripped.startswith("break") or stripped.startswith("continue")
 
     def _is_variable(self, stripped: str) -> bool:
-        pattern:re.Pattern = r'^[a-zA-Z_]\w*(\s*,\s*[a-zA-Z_]\w*)*\s*=\s*(?![=])'
+        pattern: re.Pattern = r'^[a-zA-Z_]\w*\s*=\s*[^=][^;]*?(;[a-zA-Z_]\w*\s*=\s*[^=][^;]*?)*$'
         return bool(re.match(pattern, stripped))
         
     def _is_self_attribute(self, stripped: str) -> bool:
@@ -65,8 +87,13 @@ class LineBuilder:
         return False
 
 
+    def _is_nothing(self,stripped:str):
+        return stripped == ""
 
-    def _is_comment(self, stripped : str):
+    def _is_single_comment(self, stripped : str):
+        return  stripped.startswith("#")
+    
+    def _is_multiline_comment(self,stripped:str):
         return stripped.startswith('"""')
 
     def is_indented(self,line: str) -> bool:
